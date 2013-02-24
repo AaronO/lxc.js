e7f87ecf96b445fc4cfa3d632418e107189c51d2
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-02-23 16:04:57 -0800

    Add removetemp test

---
+++ tests/removetemp.js
@@ -0,0 +1,23 @@
+var lxc = require('../lib/lxc.js').lxc;
+
+lxc.all().then(function (instances) {
+    var tempInstances = instances.filter(function(instance) {
+        return instance.search('temp') !== -1;
+    });
+
+    tempInstances.forEach(function(instance) {
+        console.log('Destroying', instance);
+
+        // Destroy
+        lxc.destroy({
+            name: instance
+        })
+        .then(function() {
+            console.log('Destryed', instance);
+        })
+        .fail(function() {
+            console.log('Failed', instance);
+        });
+    });
+
+});
